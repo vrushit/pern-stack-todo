cca7deceaa24b4a144de8187dc6faedd4cf3c409
vrushit <bharuchavrushit@ymail.com>
2021-03-23 17:59:45 +0530

    Delete Message

--- server/index.js
+++ server/index.js
@@ -78,7 +78,9 @@ app.delete("/todos/:id", async (req, res)=>{
     try{
             const{id} = req.params;
             const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1", [id]);
-    }
+    
+            res.json("Todo was deleted");
+        }
     catch(err){
         console.log(err.message)
     }
