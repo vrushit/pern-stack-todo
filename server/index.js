41ebba37d4b347b24ec581c94fbf34618289e42e
vrushit <bharuchavrushit@ymail.com>
2021-03-23 17:35:12 +0530

    Changes

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ app.post("/todos", async(req, res)=>{
 
     const {description} = req.body;
     const newTodo = await pool.query(
-        "INSERT INTO todo (description) VALUES($1)",
+        "INSERT INTO todo (description) VALUES($1) RETURNING *",
         [description] 
     );
 
