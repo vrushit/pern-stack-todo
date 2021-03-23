f606692db691664cabc000cc8843882510b747f3
vrushit <bharuchavrushit@ymail.com>
2021-03-23 22:39:32 +0530

    Await async

--- client/src/Components/ListTodo.js
+++ client/src/Components/ListTodo.js
@@ -6,7 +6,9 @@ function ListTodo() {
         try{
 
             const response = await fetch("http://localhost:5000/todos")
+            const jsonData = await response.json();
 
+            console.log(jsonData)
 
         }
         catch(err){
