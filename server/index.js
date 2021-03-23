9b46b60e1c71e04097a6b01e8aedcdbdcffc9112
vrushit <bharuchavrushit@ymail.com>
2021-03-23 17:52:20 +0530

    Update Queries

--- server/index.js
+++ server/index.js
@@ -61,7 +61,9 @@ app.get("/todos/:id", async(req, res)=>{
 
 app.put("/todos/:id", async(req, res) => {
     try{
-        
+        const{id} = req.params;
+        const {description} = req.body;
+        const updateTodo = await pool.query("UPDATE todo SET description = $1 WHERE todo_id = $2",[description, id]);
     }
     catch(err){
         console.error(err.message)
