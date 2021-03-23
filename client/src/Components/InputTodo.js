639f89bc90d71f8620edf8011884fcbaa5cf8f2c
vrushit <bharuchavrushit@ymail.com>
2021-03-23 22:23:49 +0530

    on change

--- client/src/Components/InputTodo.js
+++ client/src/Components/InputTodo.js
@@ -10,7 +10,7 @@ function InputTodo () {
                Pern Todo List
            </h1>
            <form className="d-flex mt-5">
-            <input type="text" className="form-control"/>
+            <input type="text" className="form-control" value={description} onChange={e => setDescription(e.target.value)}/>
             <button className="btn btn-success">Add</button>
            </form>
        </Fragment>
