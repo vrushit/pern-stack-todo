dbc861fe1f31c2276c0f6b591a6ceade59574f93
vrushit <bharuchavrushit@ymail.com>
2021-03-23 22:26:36 +0530

    POST method

--- client/src/Components/InputTodo.js
+++ client/src/Components/InputTodo.js
@@ -4,6 +4,21 @@ function InputTodo () {
 
     const [description, setDescription] = useState("");
 
+    const onSubmitForm = async(e) => {
+        e.preventDefault();
+        try{
+        const body = {description};
+        const response = fetch("http://localhost:5000/todos", {
+            method: "POST",
+            headers:{"Content-Type": "application/json"},
+            body: JSON.stringify(body)
+        })
+        }
+        catch(err){
+            console.error(err.message);
+        }
+    }
+
     return (
        <Fragment>
            <h1 className="text-center mt-5">
