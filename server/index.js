532e4793670ae6ef417fec74033a8fe6c25dbacf
vrushit <bharuchavrushit@ymail.com>
2021-03-23 15:43:21 +0530

    Comments for routes

--- server/index.js
+++ server/index.js
@@ -3,8 +3,24 @@ const app = express();
 const cors = require("cors");
 const pool = require("./db");
 
+
+//middleware
 app.use(cors());
 app.use(express.json())
+
+//Routes///
+
+//Create a todo
+
+//Fetch All todo
+
+
+//Fetch a Todo
+
+//Update a Todo
+
+//Delete a Todo
+
 
 
 app.listen(5000, () => {
