dae8eaa068c146d9591bcf888671a2dcfc51282b
vrushit <bharuchavrushit@ymail.com>
2021-03-23 22:32:40 +0530

    Calling List Todos

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React, {Fragment} from "react";
 import './App.css';
 import InputTodo from "./Components/InputTodo";
+import ListTodod from "./Components/ListTodo";
 function App() {
   return (
     <Fragment>
       <div className="container">
       <InputTodo />
+      <ListTodod/>
       </div>
 
     </Fragment>
