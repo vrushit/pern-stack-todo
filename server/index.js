d2c7af15cd5fbb1854243f232096b4a6828d2c7b
vrushit <bharuchavrushit@ymail.com>
2021-03-23 17:56:37 +0530

    Delete Query

--- server/index.js
+++ server/index.js
@@ -76,7 +76,8 @@ app.put("/todos/:id", async(req, res) => {
 
 app.delete("/todos/:id", async (req, res)=>{
     try{
-
+            const{id} = req.params;
+            const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1", [id]);
     }
     catch(err){
         console.log(err.message)
