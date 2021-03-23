e847eb59a3ae21c9013255bad4de73bd64b22cc5
vrushit <bharuchavrushit@ymail.com>
2021-03-23 22:53:41 +0530

    Edit Modal

--- client/src/Components/EditTodo.js
+++ client/src/Components/EditTodo.js
@@ -3,7 +3,30 @@ import React, { Fragment } from 'react'
 function EditTodo() {
     return (
         <Fragment>
-            
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">
+  Open modal
+</button>
+
+<div class="modal" id="myModal">
+  <div class="modal-dialog">
+    <div class="modal-content">
+
+      <div class="modal-header">
+        <h4 class="modal-title">Modal Heading</h4>
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+      </div>
+
+      <div class="modal-body">
+        Modal body..
+      </div>
+
+      <div class="modal-footer">
+        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+      </div>
+
+    </div>
+  </div>
+</div>
         </Fragment>
     )
 }
