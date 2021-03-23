d5096967a3ab037dc5dc016b67221e705aeec8de
vrushit <bharuchavrushit@ymail.com>
2021-03-23 22:48:04 +0530

    Delete Method

--- client/src/Components/ListTodo.js
+++ client/src/Components/ListTodo.js
@@ -20,6 +20,23 @@ function ListTodo() {
         }
     }
 
+    //delete function
+    const deleteTodo = async(id) => {
+        try{
+
+            const deleteTodo = await fetch(`http://localhost:5000/todos/${id}`, {
+                method:"DELETE",
+                
+            });
+
+
+        }
+        catch(err)
+        {
+            console.error(err.message)
+        }
+    }
+
     useEffect(() => {
         getTodos();
     }, []);
@@ -36,10 +53,10 @@ function ListTodo() {
     <tbody>
         
         {todos.map(todo => (
-            <tr>
+            <tr key={todo.todo_id}>
                 <td>{todo.description}</td>
                 <td>Edit</td>
-                <td><button className="btn btn-danger">Delete</button></td>
+                <td><button className="btn btn-danger" onClick={() => deleteTodo(todo.todo_id)}>Delete</button></td>
 
             </tr>
         ))}
