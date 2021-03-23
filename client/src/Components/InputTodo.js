0ba7a3d51d57d6db8b34f9ea3e83c8e98f9dfe7f
vrushit <bharuchavrushit@ymail.com>
2021-03-23 22:20:10 +0530

    Form

--- client/src/Components/InputTodo.js
+++ client/src/Components/InputTodo.js
@@ -6,6 +6,10 @@ const inputTodo=()=> {
            <h1 className="text-center mt-5">
                Pern Todo List
            </h1>
+           <form className="d-flex mt-5">
+            <input type="text" className="form-control"/>
+            <button className="btn btn-success">Add</button>
+           </form>
        </Fragment>
     )
 }
