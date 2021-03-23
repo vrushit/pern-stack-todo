59260e5cece4d21c1885af172b8a85d8f1dc2ecf
vrushit <bharuchavrushit@ymail.com>
2021-03-23 17:44:21 +0530

    Changes

--- server/index.js
+++ server/index.js
@@ -48,7 +48,8 @@ app.get("/todos/:id", async(req, res)=>{
     try{
 
         const {id}= req.params;
-        const todo = await pool.query("SELECT * FROM todo WHERE ")
+        const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [id])
+        res.json(todo)
 
     }
     catch(err){
